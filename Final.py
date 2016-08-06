3769e351defcd6705bcea6c5a4c3c88fe567ec16
Devin Rosenthal <devin.rosenthal1@gmail.com>
2016-08-06 11:09:58 -0700

    Implemented the rest of discard without proper AI.
    Please note recursion is not working for some reason

--- Final.py
+++ Final.py
@@ -85,41 +85,88 @@ def ptotal():
 	print("\nThe current total is "+str(total))
 
 def possible_discard(deck):
+	global total
 	for i in deck:
 		if(int(cardvals[str([i][0][0])]) <= 31 - total):
 			#print(get_value_of_card([i][0]))  #<-Used for testing
 			return True
 	return False
 
+go=0
+last_card=0
 
 def discard_round(turn,total):
-	if(turn==1):
-		if(possible_discard(player_hand)):
-			print("\nHere is your hand") 
-			print(convert_card_list_to_symbols(player_hand))
-			ptotal()
-			temp=input("Which card would you like to play?")
-			temp = int(temp)
-			if(temp>len(player_remaining) or temp<1):
-				print("The number that you have entered is out of range.")
-				discard_round(1,total)
-			if(int(get_value_of_card(player_remaining[temp-1])) + total > 31):
-				print("The calue of that card is too high. The highest that the total can be is 31")
-				discard_round(1,total)
+	global go
+	if(len(player_remaining)==0 and len(comp_remaining)==0):
+		print("There are no remaining cards. Now scoring")
+	else:
+		if(go==2):
+			if(last_card==1):
+				print("You placed the last card. You get 1 point!")
+				player_score+=1
 			else:
-				remove_number_n_from_p_and_place_in_x(temp,player_remaining,[])
-				total = int(get_value_of_card(player_remaining[temp-1])) + total
-				if(total==15):
-					print("You got the total to 15! You get 2 points!")
-					player_score+=2
-				if(total==31):
-					print("You got the total to 31! You get 2 points!")
-					player_score+=2
-					total=0
-				#discard_round(2,total)
+				print("The computer placed the last card and now gets a point")
+				computer_score+=1
+		if(turn==1):
+			if(len(player_remaining)==0):
+				print("You have run out of cards")
+				discard_round(2,total)
+			if(possible_discard(player_remaining)):
+				print("\nHere is your hand") 
+				print(convert_card_list_to_symbols(player_remaining))
+				ptotal()
+				temp=input("Which card would you like to play?\n")
+				temp = int(temp)
+				if(temp>len(player_remaining) or temp<1):
+					print("The number that you have entered is out of range.")
+					discard_round(1,total)
+				if(int(get_value_of_card(player_remaining[temp-1])) + total > 31):
+					print("The calue of that card is too high. The highest that the total can be is 31")
+					discard_round(1,total)
+				else:
+					print("You have played the "+str(convert_card_list_to_symbols([player_remaining[temp-1]])))
+					remove_number_n_from_p_and_place_in_x(temp,player_remaining,[])
+					total = int(get_value_of_card(player_remaining[temp-1])) + total
+					last_card=1
+					if(total==15):
+						print("You got the total to 15! You get 2 points!")
+						player_score+=2
+					if(total==31):
+						print("You got the total to 31! You get 2 points!")
+						player_score+=2
+						total=0 
+					discard_round(2,total)
+			else:
+				print("No possible moves. It is now the computer\'s turn")
+				go+=1
+				discard_round(2,total)
 		else:
-			print("No possible moves. It is now the computer\'s turn")
-			discard_round(2,total)
+			if(len(comp_remaining)==0):
+				print("The computer has run out of cards")
+				discard_round(1,total)
+			elif(possible_discard(comp_remaining)):
+				temp=randint(1,len(comp_remaining))			
+				if(temp>len(comp_remaining) or temp<1):
+					discard_round(2,total)
+					if(int(get_value_of_card(comp_remaining[temp-1])) + total > 31):
+						discard_round(1,total)
+					else:
+						print("The computer has played the "+str(convert_card_list_to_symbols(comp_remaining[temp-1])))
+						remove_number_n_from_p_and_place_in_x(temp,comp_remaining,[])
+						total = int(get_value_of_card(comp_remaining[temp-1])) + total
+						last_card=2 				
+						if(total==15): 
+							print("The comuputer got the total to 15 and has recieved 2 points.")
+							computer_score+=2
+						if(total==31): 
+							print("The comuputer got the total to 31 and has recieved 2 points.")
+							computer_score+=2
+							total=0
+						discard_round(1,total)
+			else:
+				go+=1
+				print("The computer said go. It is now your turn.")
+				discard_round(1,total)
 
 start()
 
@@ -131,10 +178,10 @@ player_hand=deal_n_using_deck(6)
 comp_hand=deal_n_using_deck(4)
 crib=deal_n_using_deck(2) #Auto adds computer's cards to crib - May add AI to do this instead (To make it more advanced)
 start_card=deal_n_using_deck(1)[0]
-total=get_value_of_card(start_card)
 player_score=0
 computer_score=0
 turn=1
+total=0
 
 print("\nHere is your hand. Please say the number (1-6) of one of the cards that you would like to place into the crib.\nPlease note \'T\' stands for 10 and that entering a number higher than your hand will use your last card.")
 print(convert_card_list_to_symbols(player_hand))
@@ -144,11 +191,11 @@ print("\nPlease place one more card into the crib.")
 print(convert_card_list_to_symbols(player_hand))
 temp=input("")
 remove_number_n_from_p_and_place_in_x(int(temp),player_hand,crib)
-print("\nHere is your hand")
-print(convert_card_list_to_symbols(player_hand)) 
-print("\nThe starting card is "+str(convert_card_list_to_symbols([start_card])))
+#print("\nHere is your hand")
+#print(convert_card_list_to_symbols(player_hand)) 
+#print("\nThe starting card is "+str(convert_card_list_to_symbols([start_card])))
 player_remaining=player_hand[:]
 comp_remaining=comp_hand[:]
-discard_round(turn,total)
+discard_round(turn,0)
 
 
